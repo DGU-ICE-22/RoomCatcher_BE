4ce54d5c3518399003b0fd8da7e89ca05ebe208a
5hseok <ohhs1010@gmail.com>
2024-04-04 13:42:09 +0900

    :tada: 프로젝트 spring.io.start로 생성

---
+++ src/main/java/com/roomcatcher/RoomCatcher/RoomCatcherApplication.java
@@ -0,0 +1,13 @@
+package com.roomcatcher.RoomCatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomCatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomCatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roomcatcher/RoomCatcher/RoomCatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomcatcher.RoomCatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomCatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
