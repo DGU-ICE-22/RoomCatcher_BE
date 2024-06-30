c402e47393922939e5b90951be6877f7399b8815
eeddiinn <gdw4013@naver.com>
2024-07-01 01:02:07 +0900

    [FEAT] Tag 엔티티 설계 #2

---
+++ src/main/java/com/roomcatcher/RoomCatcher/domain/Tag.java
@@ -0,0 +1,32 @@
+package com.roomcatcher.RoomCatcher.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+
+    @Id
+    @Column(name = "tagId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String tagName;
+
+    @OneToMany(mappedBy = "tag")
+    private List<UserTag> userTags;
+
+    @OneToMany(mappedBy = "tag")
+    private List<ProductTag> productTags = new ArrayList<>();
+
+    @OneToMany(mappedBy = "tag")
+    private List<UserTypeTag> userTypeTags = new ArrayList<>();
+}
