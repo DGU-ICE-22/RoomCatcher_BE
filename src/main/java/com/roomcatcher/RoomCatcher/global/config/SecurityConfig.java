814f09bd2e8bc7d812086c19c7e6b8319d9f994e
eeddiinn <gdw4013@naver.com>
2024-08-20 18:26:02 +0900

    [FIX] 비밀번호 암화화 하여 저장하도록 수정 #5

--- src/main/java/com/roomcatcher/RoomCatcher/global/config/SecurityConfig.java
+++ src/main/java/com/roomcatcher/RoomCatcher/global/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.roomcatcher.RoomCatcher.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -19,5 +21,10 @@ public class SecurityConfig {
             )
             .formLogin(formLogin -> formLogin.disable());   // 기본 로그인 폼 비활성화
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();  // 비밀번호 암호화를 위한 BCryptPasswordEncoder 빈 등록
     }
 }
--- src/main/java/com/roomcatcher/RoomCatcher/service/UserService.java
+++ src/main/java/com/roomcatcher/RoomCatcher/service/UserService.java
@@ -5,14 +5,18 @@ import com.roomcatcher.RoomCatcher.dto.request.UserCreateRequest;
 import com.roomcatcher.RoomCatcher.dto.response.UserCreateResponse;
 import com.roomcatcher.RoomCatcher.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j // 다양한 로깅 프레임워크(예: Logback, Log4j, JUL)를 사용할 수 있게하는 어노테이션
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     @Transactional
     public UserCreateResponse signUp(UserCreateRequest userCreateRequest) {
 
@@ -20,7 +24,7 @@ public class UserService {
                         .userName(userCreateRequest.getName())
                         .userBirth(userCreateRequest.getBirth())
                         .email(userCreateRequest.getEmail())
-                        .password(userCreateRequest.getPassword())
+                        .password(passwordEncoder.encode(userCreateRequest.getPassword())) // 비밀번호를 암호화하여 저장
                         .userSex(null)
                         .userImage(null)
                         .userType(null)
