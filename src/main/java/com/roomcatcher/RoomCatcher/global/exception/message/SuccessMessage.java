d3c18fe9bcdd5c8a6ea9cc82d4fb01ec6886e012
5hseok <ohhs1010@gmail.com>
2024-08-24 23:26:16 +0900

    :sparkles: [FEAT] 사용자 타입 추가 API 구현 태그도 user_tag 테이블에 추가하는거 UserTypeService에 추가구현 해야 함.

--- src/main/java/com/roomcatcher/RoomCatcher/global/exception/message/SuccessMessage.java
+++ src/main/java/com/roomcatcher/RoomCatcher/global/exception/message/SuccessMessage.java
@@ -13,7 +13,7 @@ public enum SuccessMessage {
     SIGNIN_SUCCESS(HttpStatus.CREATED.value(),"로그인에 성공하였습니다."),
     LOGOUT_SUCCESS(HttpStatus.OK.value(),"로그아웃에 성공하였습니다."),
     ACCOUNT_DELETION_SUCCESS(HttpStatus.OK.value(),"회원 탈퇴에 성공하였습니다."),
-    ;
+    CREATE_USER_TYPE(HttpStatus.CREATED.value(), "사용자 유형 생성에 성공하였습니다."),;
     private final int code;
     private final String message;
 }
